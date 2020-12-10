1e2358f8138d4b25c6abe046903bf4d942d42c47
AndrasMarro <72513821+AndrasMarro@users.noreply.github.com>
2020-12-10 13:47:54 +0100

    Projektdateien hinzufügen.

---
+++ MustCanDo2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MustCanDo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextVerwaltung tv = new TextVerwaltung();
+            tv.AddTexte("Hallo");
+            tv.AddTexte("tolle");
+            tv.AddTexte("3CHIT");
+            tv.PrintText();
+            Console.WriteLine(tv.ReplaceText("Hallo", "Hi"));
+            tv.PrintText();
+            Console.WriteLine(tv.ReplaceText("Hi", "liebe"));
+            tv.PrintText();
+            Console.ReadKey();
+        }
+    }
+}
